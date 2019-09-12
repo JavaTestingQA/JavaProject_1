06f0db1d04895f3dd1a149853368b87878b83f34
Alex <myjavaaccount@mail.ru>
2019-09-12 23:33:52 +0300

    Удален и заново клонирован репозиторий.
    Запуск из IDEA перестал сопровождаться ошибкой.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstJavaProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstJavaProgram.java
@@ -9,12 +9,12 @@ public class MyFirstJavaProgram {
 
         Square s = new Square();
         s.l = 5;
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+        System.out.println("РџР»РѕС‰Р°РґСЊ РєРІР°РґСЂР°С‚Р° СЃРѕ СЃС‚РѕСЂРѕРЅРѕР№ " + s.l + " = " + area(s));
 
         Rectangle r = new Rectangle();
         r.a = 4;
         r.b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+        System.out.println("РџР»РѕС‰Р°РґСЊ РїСЂСЏРјРѕСѓРіРѕР»СЊРЅРёРєР° СЃРѕ СЃС‚РѕСЂРѕРЅР°РјРё " + r.a + " Рё " + r.b + " = " + area(r));
     }
 
     public static void hello(String sombody) {
@@ -25,7 +25,8 @@ public class MyFirstJavaProgram {
         return s.l * s.l;
     }
 
-    public static double area(Rectangle r) {
+    public static double area(Rectangle r)
+    {
         return r.a * r.b;
     }
 }
