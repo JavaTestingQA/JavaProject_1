36c170feb93a9472dfaf499e2638a5d09b606911
Alex <myjavaaccount@mail.ru>
2019-09-16 21:23:14 +0300

    Добавлено заполнение атрибутов объектов через конструктор

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -4,14 +4,9 @@ public class Distance {
 
     public static void main(String[] Args) {
 
-        Point p1 = new Point();
-        p1.x = 3;
-        p1.y = -3;
-
-        Point p2 = new Point();
-        p2.x = -6;
-        p2.y = 2;
-
+        Point p1 = new Point(3, -3);
+        Point p2 = new Point(-6, 2);
+        
         System.out.println("Расстояние между двумя точками " + "P1 (x = " + p1.x + ", y = " + p1.y + ")" + " и " + "P2" + " (x = " + p2.x + ", y = " + p2.y + ")" + " = " + distance(p1, p2));
     }
 
