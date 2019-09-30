f51f3f2fbdbe8ee23365c9c0984c7c329e5dd583
Alex <myjavaaccount@mail.ru>
2019-09-30 23:28:29 +0300

    Переработан тест модификации контактов.
    1. Убран бесполезный вызов метода выбора контакта (отметки чекбокса).
    2. Вместо него добавлен метод перехода (по сути обновление страницы) на главную страницу контактов.
    
    Тест модификации контакта успешно отрабатывает.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -7,7 +7,7 @@ public class ContactModificationTests extends TestBase {
 
     @Test
     public void testContactModification() {
-        app.getContactHelper().selectContact();
+        app.getNavigationHelper().goToHomePage();
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new ContactData("Александр", "Дрозд", "г. Москва, улица Рябиновая, дом 7, корпус 5.", "8-978-485-74-11", "alexqamail@qa.ru"));
         app.getContactHelper().submitContactModification();
