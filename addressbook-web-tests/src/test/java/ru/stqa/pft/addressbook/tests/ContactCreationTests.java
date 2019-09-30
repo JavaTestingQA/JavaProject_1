fc618fa7b6cb68c295d869489064edd24da57e73
Alex <myjavaaccount@mail.ru>
2019-09-30 22:42:45 +0300

    Доработан метод type (HelperBase) - пропуск ввода значения в поле если оно совпадает с уже имеющемся в поле
    Тесты модификации групп и контактов (запущенные в дебаггере) наглядно показывают работу и пользу от данной оптимизации!

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -8,7 +8,7 @@ public class ContactCreationTests extends TestBase{
     @Test
     public void testContactCreation() throws Exception {
         app.getContactHelper().initContactCreation();
-        app.getContactHelper().fillContactForm(new ContactData("Александр", "Дрозд", "г. Москва, улица Рябиновая, дом 7, корпус 5.", "8-978-485-74-11", null));
+        app.getContactHelper().fillContactForm(new ContactData("Александр", "Дрозд", "г. Москва, улица Рябиновая, дом 7, корпус 5.", "8-978-485-74-11", "alexqamail@qa.ru"));
         app.getContactHelper().submitContactCreation();
         app.getContactHelper().returnToHomePage();
         app.getSessionHelper().logout();
