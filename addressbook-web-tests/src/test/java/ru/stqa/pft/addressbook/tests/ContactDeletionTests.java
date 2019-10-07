1a3b38f3ab37a16e68951a45d7750ab88bfab25d
Alex <myjavaaccount@mail.ru>
2019-10-07 23:28:27 +0300

    Тест удаления контактов переработан и использует сравнение коллекций
    Тест удаления коллекций успешно проходит как при наличии группы, так и при её отсутствии.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -6,21 +6,24 @@ import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.List;
 
-public class ContactDeletionTests extends TestBase{
+public class ContactDeletionTests extends TestBase {
 
-  @Test
-  public void testContactDeletion() throws Exception {
-    app.getNavigationHelper().goToHomePage();
-    if (! app.getContactHelper().isThereAContact()) {
-      app.getContactHelper().createContact(new ContactData("Александр", "Дрозд", "г. Москва, улица Рябиновая, дом 7, корпус 5.", "8-978-485-74-11", "alexqamail@qa.ru", "test1"), true);
+    @Test
+    public void testContactDeletion() throws Exception {
+        app.getNavigationHelper().goToHomePage();
+        if (!app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().createContact(new ContactData("Александр", "Дрозд", "г. Москва, улица Рябиновая, дом 7, корпус 5.", "8-978-485-74-11", "alexqamail@qa.ru", "test1"), true);
+        }
+        List<ContactData> before = app.getContactHelper().getContactList();
+        app.getContactHelper().selectContact(before.size() - 1);
+        app.getContactHelper().deleteSelectedContact();
+        app.getContactHelper().acceptContactDeleting();
+        app.getNavigationHelper().goToHomePage();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size() - 1);
+        app.getSessionHelper().logout();
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
     }
-    List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().selectContact(before.size() - 1);
-    app.getContactHelper().deleteSelectedContact();
-    app.getContactHelper().acceptContactDeleting();
-    app.getNavigationHelper().goToHomePage();
-    List<ContactData> after = app.getContactHelper().getContactList();
-    Assert.assertEquals(after.size(), before.size() - 1);
-    app.getSessionHelper().logout();
-  }
 }
