3f854068807041c724f9d09abae64836a7874b71
Alex <myjavaaccount@mail.ru>
2019-11-03 21:14:16 +0300

    Реализовано получение почты на внешний email-сервер по Telnet (Лекция 8.7)
    Тест регистрации пользователя Mantis переведен на использование почтового сервера James по Telnet.
    
    Тест успешно отрабатывает.

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -15,19 +15,22 @@ import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase{
 
-    @BeforeMethod
+    //@BeforeMethod
     public void startMailServer() {
         app.mail().start();
     }
 
     @Test
-    public void testRegistration() throws IOException, MessagingException {
+    public void testRegistration() throws IOException, MessagingException, javax.mail.MessagingException {
         long now = System.currentTimeMillis();
         String user = String.format("user%s", now);
         String password = "password";
-        String email = String.format("user%s@localhost.localdomain", now);
+        //String email = String.format("user%s@localhost.localdomain", now); С таким форматом адреса почты из лекции 8.7 - почта НЕ приходит!
+        String email = String.format("user%s@localhost", now);
+        app.james().createUser(user, password);
         app.registration().start(user, email);
-        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        //List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
         app.registration().finish(confirmationLink, password);
         assertTrue(app.newSession().login(user, password));
@@ -39,7 +42,7 @@ public class RegistrationTests extends TestBase{
         return regex.getText(mailMessage.text);
     }
 
-    @AfterMethod(alwaysRun = true)
+    //@AfterMethod(alwaysRun = true)
     public void stopMailServer() {
         app.mail().stop();
     }
